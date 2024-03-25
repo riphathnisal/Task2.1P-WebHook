9521e1c44381c24408b7854b3627d098f38abf16
riphathnisal <164106868+riphathnisal@users.noreply.github.com>
2024-03-25 16:10:32 +1100

    Add files via upload

---
+++ WriteMultipleFields.ino
@@ -0,0 +1,91 @@
+/*
+  WriteMultipleFields
+  
+  Description: Writes values to fields 1,2,3,4 and status in a single ThingSpeak update every 20 seconds.
+  
+  Hardware: Arduino MKR1000
+  
+  !!! IMPORTANT - Modify the secrets.h file for this project with your network connection and ThingSpeak channel details. !!!
+  
+  Note:
+  - Requires WiFi101 library version 0.15.3 or newer.
+  - This example is written for a network using WPA encryption. For WEP or WPA, change the WiFi.begin() call accordingly.
+  
+  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize, and 
+  analyze live data streams in the cloud. Visit https://www.thingspeak.com to sign up for a free account and create a channel.  
+  
+  Documentation for the ThingSpeak Communication Library for Arduino is in the README.md folder where the library was installed.
+  See https://www.mathworks.com/help/thingspeak/index.html for the full ThingSpeak documentation.
+  
+  For licensing information, see the accompanying license file.
+  
+  Copyright 2020, The MathWorks, Inc.
+*/
+
+#include <WiFiNINA.h>
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#include <DHT.h>  // Include the DHT library
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+#define DHTPIN 2 // Define the ditgital pin for DHT sensor
+#define DHTTYPE DHT11 //Define the type of DHT sensor
+
+DHT dht(DHTPIN, DHTTYPE); //Creating a DHT object for temperature and humidity
+
+void setup() {
+  Serial.begin(115200);      // Initialize serial 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  ThingSpeak.begin(client);  // Initialize ThingSpeak 
+  dht.begin();
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+
+  float humidity = dht.readHumidity(); //read humidity from DHT
+  float temperature = dht.readTemperature(); //read temperature from DHT
+
+  // Check if sensor readings are valid 
+   if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    delay(2000);
+    return;
+  }
+
+//Set values of feilds 1 and 2 in the Thinkspeak Channel
+  ThingSpeak.setField(1, temperature);
+  ThingSpeak.setField(2, humidity);
+
+//Send the data to Thinkspeak and get the HTTP repsonse code.
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if (x == 200) {
+    Serial.println("Channel update successful.");
+  } else {
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "S22Nisal"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "nisal2003"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID  2474427			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "JJF658240B93PG0Y"   // replace XYZ with your channel write API Key
